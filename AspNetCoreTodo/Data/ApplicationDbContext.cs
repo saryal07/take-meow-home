6db66ec6b275f23fb959d4f4cccfb08f6723a2a4
Sajan Aryal <timber.npl2018@gmail.com>
2024-04-20 15:44:23 -0500

    push the folder with code

---
+++ AspNetCoreTodo/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using AspNetCoreTodo.Models;
+
+namespace AspNetCoreTodo.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // ...
+    }
+
+    public DbSet<TodoItem> Items { get; set; }
+}
